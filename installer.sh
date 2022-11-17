422157af7cc749ed287005d232ec19045c1688a5
emil-nabil333 <elbrinselomda@yahoo.com>
2022-11-17 19:43:35 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
-#!/bin/sh # 
- # # Command: wget https://raw.githubusercontent.com/emilnabil/picon-nilesat/main/installer.sh -qO - | /bin/sh # # ########################################### ###########################################  
-MY_URL=https://raw.githubusercontent.com/emilnabil/picon-nilesat/main/  
+#!/bin/sh 
+#
+MY_URL="https://raw.githubusercontent.com/emilnabil/picon-nilesat/main"  
 echo "******************************************************************************************************************"
 echo "    download and install picon  "
 echo "============================================================================================================================="
@@ -8,27 +8,45 @@ echo "==========================================================================
 echo "         install picon-nilesat    "
 cd /tmp
 set -e 
-wget -q  "https://raw.githubusercontent.com/emilnabil/picon-nilesat/main/picon1.tar.gz"
+wget -q  "https://raw.githubusercontent.com/emilnabil/picon-nilesat/main/picon.zip"
 wait
-tar -xzf picon1.tar.gz  -C /
+unzip -qn ./picon.zip
+    rm -f /tmp/picon.zip
+wait
+
+if [ -d /media/mmc/picon/ ] ; then
+    cp -rf /tmp/picon/* /media/mmc/picon/
+	echo "Coping Picon To /media/mmc/picon/"
+    else
+    echo ""
+fi
+
+if [ -d /media/usb/picon/ ] ; then
+    cp -rf /tmp/picon/* /media/usb/picon/
+	echo "Coping Picon To /media/usb/picon/"
+    else
+    echo ""
+fi
+
+if [ -d /media/hdd/picon/ ] ; then
+    cp -rf /tmp/picon/* /media/hdd/picon/
+	echo "Coping Picon To /media/hdd/picon/"
+    else
+    echo ""
+fi
+
+if [ -d /usr/share/enigma2/picon/ ] ; then
+    cp -rf /tmp/picon/* /usr/share/enigma2/picon/
+	echo "Coping Picon To /usr/share/enigma2/picon/"
+    else
+    echo ""
+fi
 wait
 cd ..
 set +e
-rm -f /tmp/picon1.tar.gz
+rm -rf /tmp/picon
 echo "==========================================================================================================================="
-echo "         install picon-nilesat    "
-cd /tmp
-set -e 
-wget -q  "https://raw.githubusercontent.com/emilnabil/picon-nilesat/main/picon2.tar.gz"
-wait
-tar -xzf picon2.tar.gz  -C /
-wait
-cd ..
-set +e
-rm -f /tmp/picon2.tar.gz
-sleep 2;
-echo "" 
-echo "" 
+
 echo "****************************************************************************************************************************"
 echo "# PICON  INSTALLED SUCCESSFULLY #"
 echo "
@@ -36,27 +54,10 @@ echo " "*********************************************************"
 	echo "********************************************************************************"
 echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
 sleep 4;
-	echo '========================================================================================================================='
-###########################################                                                                                                                  
-echo ". >>>>         RESTARING     <<<<"
+	echo '========================================================================================================================='                                                                                                             
 echo "**********************************************************************************"
 wait
-killall -9 enigma2
 exit 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
